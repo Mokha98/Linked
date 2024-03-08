8ebf1fb74dfa86c230ba8542a567b0e6f55bebfd
Mohammad Khalaf <mokha9898@gmail.com>
2024-03-08 15:44:46 +0300

    setup .net core skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController:ControllerBase
+{
+    private readonly DataContext _dbContext;
+    public UsersController(DataContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _dbContext.AppUsers.ToArrayAsync();
+        return users;
+    }
+
+   [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var users = await _dbContext.AppUsers.FindAsync(id);
+        return users;
+    }
+
+}
